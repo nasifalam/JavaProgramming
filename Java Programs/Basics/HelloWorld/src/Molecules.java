73bcf92b35ee2040fbb141a2b5246bfe5fdc8864
unknown <nasif.alam@mail.mcgill.ca>
2021-01-07 13:47:07 -0500

    adding two files

---
+++ Java Programs/Basics/HelloWorld/src/Molecules.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+public class Molecules {
+	
+	public static void main(String[] args) {
+	
+	
+
+		
+		  
+		    // Create a HashMap object called capitalCities
+		    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+		    // Add keys and values (Country, City)
+		    capitalCities.put("C", "12");
+		    capitalCities.put("H", "22");
+		    capitalCities.put("O", "11");
+		    capitalCities.put("USA", "Washington DC");
+		    System.out.println(capitalCities);
+		  
+		
+		
+		/***
+		String molecule = "C12H22O11";    
+        int count = 0;    
+            
+        //Counts each character except space    
+        for(int i = 0; i < molecule.length(); i++) {    
+            if(molecule.charAt(i) == 'C')    
+                count++;    
+        }    
+            
+        //Displays the total number of characters present in the given string    
+        System.out.println("Total number of carbon in " + molecule + " is " + count);
+	 ***/
+	}
+
+
+}
